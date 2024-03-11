31496dcee66b4d51526b12a23adc538432276611
[Ink.] <[a.k.026@yandex.ru]>
2024-03-11 15:05:41 +0300

    готово

--- tabs.js
+++ tabs.js
@@ -1,6 +1,33 @@
 let div = document.createElement("div");
 
-const tabCardWrapper = document.querySelector(".tab__card-wrapper");
+
+
+const earphonesTabCardWrapper = document.querySelector('.earphones').querySelector(".tab__card-wrapper");
+const sellersTabCardWrapper = document.querySelector('.sellers').querySelector(".tab__card-wrapper");
+
+const sellersTabBtnWrapper = document.querySelector('.sellers').querySelector(".tab__btn-wrapper");
+
+sellersTabBtnWrapper.addEventListener("click",(event) => {
+  const target = event.target
+  const ovalButtons = sellersTabBtnWrapper.querySelectorAll('button')
+  ovalButtons.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  target.classList.add('active')
+  
+} )
+
+const earphonesTabBtnWrapper = document.querySelector('.earphones').querySelector(".tab__btn-wrapper");
+
+earphonesTabBtnWrapper.addEventListener("click",(event) => {
+  const target = event.target
+  const ovalButtons = earphonesTabBtnWrapper.querySelectorAll('button')
+  ovalButtons.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  target.classList.add('active')
+  
+} )
 
 const cardInfoList = [
   {
@@ -20,8 +47,8 @@ const cardInfoList = [
   },
 ];
 
-function getCards(cardInfoList) {
-  const arr = cardInfoList.map((cardInfo) => {
+function getCards(cardInfoList, elem) {
+  const arr = cardInfoList.forEach(cardInfo => {
     const tab = `<div class="tab__card card">
         <div class="card__product">
           <img
@@ -47,10 +74,9 @@ function getCards(cardInfoList) {
           <p>Save 60%</p>
         </div>
         </div>`;
-    return tab;
+    elem.insertAdjacentHTML("beforeend", tab);
   });
-  console.log(arr);
 }
-getCards(cardInfoList);
+getCards(cardInfoList, sellersTabCardWrapper);
 
-// tabCardWrapper.insertAdjacentHTML("afterbegin", tab);
+getCards(cardInfoList, earphonesTabCardWrapper);
